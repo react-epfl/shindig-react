e7b577ddf3b49a05def1e3661e2f10cbe21628ab
Aubry Cholleton <aubry.cholleton@epfl.ch>
2013-07-10 16:14:28 +0200

    New handler to manage activity streams. Supports simple GET requests.

--- java/social-api/src/main/java/org/apache/shindig/social/opensocial/service/ActivityStreamHandler.java
+++ java/social-api/src/main/java/org/apache/shindig/social/opensocial/service/ActivityStreamHandler.java
@@ -38,6 +38,32 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.inject.Inject;
 
+import org.json.JSONObject;
+import java.io.File;
+import java.io.*;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.ImmutableSet;
+import com.google.inject.Inject;
+import com.google.common.util.concurrent.Futures;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.nio.charset.Charset;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.ContentBody;
+
+import org.apache.commons.fileupload.FileItem;
+
+
 /**
  * <p>ActivityStreamHandler class.</p>
  */
@@ -46,6 +72,8 @@ public class ActivityStreamHandler {
 
   private final ActivityStreamService service;
   private final ContainerConfig config;
+  public static final String GRAASP_URL = System.getProperty("shindig.container_url");
+  private final String GRAASP_TOKEN = System.getProperty("graasp.token");
 
   /**
    * <p>Constructor for ActivityStreamHandler.</p>
@@ -148,6 +176,8 @@ public class ActivityStreamHandler {
   @Operation(httpMethods="GET")
   public Future<?> get(SocialRequestItem request)
       throws ProtocolException {
+        System.out.println("Get activity stream");
+        /*
     Set<UserId> userIds = request.getUsers();
     Set<String> optionalActivityIds = ImmutableSet.copyOf(request.getListParameter("activityId"));
 
@@ -175,6 +205,46 @@ public class ActivityStreamHandler {
         // TODO: add pagination and sorting support
         // getSortBy(params), getFilterBy(params), getStartIndex(params), getCount(params),
         request.getFields(), options, request.getToken());
+        */
+    //Set<String> fields = request.getFields(ActivityEntry.Field.DEFAULT_FIELDS);
+    Set<String> contextIds = request.getContextIds();
+    String contextType = request.getContextType();
+    String viewerId = request.getToken().getViewerId();
+    String size = request.getParameter("size");
+
+    // Preconditions
+    HandlerPreconditions.requireNotEmpty(contextIds, "No contextId is specified");
+    
+    CollectionOptions options = new CollectionOptions(request);
+    if(contextType == null){
+      // when contextType is not specified, consider it is a User. TODO : implement this correctly
+      if(contextIds.size() == 1){
+        String output = "";
+        HttpClient client = new DefaultHttpClient();
+        String url = GRAASP_URL+"/rest/activitystreams/"+contextIds.iterator().next()+"/@self/?token="+GRAASP_TOKEN;//+"&user="+viewerId;
+        System.out.println(url);
+        HttpGet get = new HttpGet(url);
+        get.getParams().setParameter("http.protocol.expect-continue", false);
+        try {
+          HttpResponse response = client.execute(get);
+          BufferedReader rd = new BufferedReader(new InputStreamReader(
+              response.getEntity().getContent()));
+          String line = "";
+          while ((line = rd.readLine()) != null) {
+            output = line;
+          }   
+          System.out.println(output);
+          JSONObject jsonOutput = new JSONObject(output);
+          return  Futures.immediateFuture(jsonOutput);
+        } catch (Exception e) {
+          return  Futures.immediateFuture(e);
+        }
+      }else{
+        throw new IllegalArgumentException("Cannot fetch activities for multiple contexts");
+      }
+    }else{
+        throw new IllegalArgumentException("Context type is not null");
+      }
   }
 
   /**
